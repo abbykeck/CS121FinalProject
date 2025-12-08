e8d3b8f8eda07e61f32cbf768bbcf673988b5d6f
abbykeck <abigailkeck2425@gmail.com>
2025-12-08 21:09:47 +0000

    commented out all unused data fields in Game class, fixed error with searchByTitle() where the program would throw an error if the user inputted a space in the query

--- GameLL.java
+++ GameLL.java
@@ -105,8 +105,14 @@ public class GameLL implements Serializable {
 		String userInput = "";
 		System.out.print("Enter a game title (not case sensitive): ");
 		userInput = input.nextLine();
+		String urlInput = "";
+		for (int i = 0; i < userInput.length(); i++) {
+			if (!(userInput.substring(i, i + 1).equals(" "))) {
+				urlInput += userInput.substring(i, i + 1);
+			} // end if
+		} // end for
 		String json = "";
-		String url = "https://www.cheapshark.com/api/1.0/deals?storeID=1&pageSize=10&title=" + userInput;
+		String url = "https://www.cheapshark.com/api/1.0/deals?storeID=1&pageSize=10&title=" + urlInput;
                 System.out.println("Search results:\n");
                 try {
                         /*
@@ -239,25 +245,31 @@ class GameList implements Serializable {
 	} // end getGames
 } // end GameList
 class Game implements Serializable {
-        private String internalName;
+        //private String internalName;
         private String title;
+	/*
         private String metacriticLink;
         private String dealID;
         private String storeID;
         private String gameID;
+	*/
         private String salePrice;
+	/*
         private String normalPrice;
         private String isOnSale;
         private String savings;
         private String metacriticScore;
+	*/
         private String steamRatingText;
         private String steamRatingPercent;
-        private String steamRatingCount;
+        //private String steamRatingCount;
         private String steamAppID;
+	/*
         private int releaseDate;
         private int lastChange;
         private String dealRating;
         private String thumb;
+	*/
 
         public void printGame() {
                 System.out.println("Game title: " + title);
